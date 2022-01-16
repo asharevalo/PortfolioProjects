2749b3b399597ecb9c38508eb9a7810321540cc0
AshleyArevalo <96358278+asharevalo@users.noreply.github.com>
2022-01-16 15:34:38 -0600

    Update Covid Data Exploration.sql

--- Covid Data Exploration.sql
+++ Covid Data Exploration.sql
@@ -1,9 +1,90 @@
 /*
 Covid Data Exploration
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types, Updating Tables
 
 */
+
+-- Updating Tables to remove nulls and replace with zero in value fields
+UPDATE PortfolioProject.dbo.CovidDeaths$
+SET total_cases=0
+WHERE total_cases IS NULL;
+
+UPDATE PortfolioProject..CovidVaccinations$
+SET tests_units=0
+WHERE tests_units is null;
+
+
+--Updating multiple tables and multiple columns to replace nulls with zero.
+--Converting data types to work when replacing
+
+UPDATE PortfolioProject.dbo.CovidDeaths$
+SET
+	new_deaths = COALESCE(CONVERT(float, new_deaths), 0),
+	new_deaths_smoothed = COALESCE(CONVERT(float, new_deaths_smoothed), 0),
+	total_cases_per_million = COALESCE(total_cases_per_million, 0),
+	new_cases_per_million = COALESCE(new_cases_per_million, 0),
+	new_cases_smoothed_per_million = COALESCE(new_cases_smoothed_per_million, 0),
+	total_deaths_per_million = COALESCE(CONVERT(float, total_deaths_per_million), 0),
+	new_deaths_per_million = COALESCE(CONVERT(float, new_deaths_per_million), 0),
+	new_deaths_smoothed_per_million = COALESCE(CONVERT(float, new_deaths_smoothed_per_million), 0),
+	reproduction_rate = COALESCE(CONVERT(float, reproduction_rate), 0),
+	icu_patients = COALESCE(CONVERT(float, icu_patients), 0),
+	icu_patients_per_million = COALESCE(CONVERT(float, icu_patients_per_million), 0),
+	hosp_patients = COALESCE(CONVERT(float, hosp_patients), 0),
+	hosp_patients_per_million = COALESCE(CONVERT(float, hosp_patients_per_million), 0),
+	weekly_icu_admissions = COALESCE(CONVERT(float, weekly_icu_admissions), 0),
+	weekly_icu_admissions_per_million = COALESCE(CONVERT(float, weekly_icu_admissions_per_million), 0),
+	weekly_hosp_admissions = COALESCE(CONVERT(float, weekly_hosp_admissions), 0),
+	weekly_hosp_admissions_per_million = COALESCE(CONVERT(float, weekly_hosp_admissions_per_million), 0)
+
+UPDATE PortfolioProject..CovidVaccinations$
+SET 
+	new_tests = COALESCE(CONVERT(float, new_tests), 0),
+	total_tests = COALESCE(CONVERT(float, total_tests), 0),
+	total_tests_per_thousand = COALESCE(CONVERT(float, total_tests_per_thousand), 0),
+	new_tests_per_thousand = COALESCE(CONVERT(float, new_tests_per_thousand), 0),
+	new_tests_smoothed = COALESCE(CONVERT(float, new_tests_smoothed), 0),
+	new_tests_smoothed_per_thousand = COALESCE(CONVERT(float, new_tests_smoothed_per_thousand), 0),
+	positive_rate = COALESCE(CONVERT(float, positive_rate), 0),
+	tests_per_case = COALESCE(CONVERT(float, tests_per_case), 0),
+	total_vaccinations = COALESCE(CONVERT(float, total_vaccinations), 0),
+	people_vaccinated = COALESCE(CONVERT(float, people_vaccinated), 0),
+	people_fully_vaccinated = COALESCE(CONVERT(float, people_fully_vaccinated), 0),
+	total_boosters = COALESCE(CONVERT(float, total_boosters), 0),
+	new_vaccinations = COALESCE(CONVERT(float, new_vaccinations), 0),
+	new_vaccinations_smoothed = COALESCE(CONVERT(float, new_vaccinations_smoothed), 0),
+	total_vaccinations_per_hundred = COALESCE(CONVERT(float, total_vaccinations_per_hundred), 0),
+	people_vaccinated_per_hundred = COALESCE(CONVERT(float, people_vaccinated_per_hundred), 0),
+	people_fully_vaccinated_per_hundred = COALESCE(CONVERT(float, people_fully_vaccinated_per_hundred), 0),
+	total_boosters_per_hundred = COALESCE(CONVERT(float, total_boosters_per_hundred), 0),
+	new_vaccinations_smoothed_per_million = COALESCE(CONVERT(float, new_vaccinations_smoothed_per_million), 0),
+	new_people_vaccinated_smoothed = COALESCE(CONVERT(float, new_people_vaccinated_smoothed), 0),
+	new_people_vaccinated_smoothed_per_hundred = COALESCE(CONVERT(float, new_people_vaccinated_smoothed_per_hundred), 0),
+	stringency_index = COALESCE(stringency_index, 0),
+	population_density = COALESCE(population_density, 0),
+	median_age = COALESCE(median_age, 0),
+	aged_65_older = COALESCE(aged_65_older, 0),
+	aged_70_older = COALESCE(aged_70_older, 0),
+	gdp_per_capita = COALESCE(gdp_per_capita, 0),
+	extreme_poverty = COALESCE(CONVERT(float, extreme_poverty), 0),
+	cardiovasc_death_rate = COALESCE(cardiovasc_death_rate, 0),
+	diabetes_prevalence = COALESCE(diabetes_prevalence, 0),
+	female_smokers = COALESCE(CONVERT(float, female_smokers), 0),
+	male_smokers = COALESCE(CONVERT(float, male_smokers), 0),
+	handwashing_facilities = COALESCE(handwashing_facilities, 0),
+	hospital_beds_per_thousand = COALESCE(hospital_beds_per_thousand, 0),
+	life_expectancy = COALESCE(life_expectancy, 0),
+	human_development_index = COALESCE(human_development_index, 0),
+	excess_mortality_cumulative_absolute = COALESCE(CONVERT(float, excess_mortality_cumulative_absolute), 0),
+	excess_mortality_cumulative = COALESCE(CONVERT(float, excess_mortality_cumulative), 0),
+	excess_mortality = COALESCE(CONVERT(float, excess_mortality), 0),
+	excess_mortality_cumulative_per_million = COALESCE(CONVERT(float, excess_mortality_cumulative_per_million), 0)
+
+SELECT new_tests
+FROM PortfolioProject..CovidVaccinations$
+WHERE new_tests is null
+	
 
 Select *
 From PortfolioProject.dbo.CovidDeaths$
